047457e0abb0c35a8fa72a718a225fda05dd34ec
karameru1729 <karameru1729@gmail.com>
2026-04-07 18:23:33 +0900

    add:loading機能の追加

--- components/EditorWrapper.tsx
+++ components/EditorWrapper.tsx
@@ -7,7 +7,6 @@ import React from 'react';
 // ※パス('./Editor')は、Editor.tsxがある実際の場所に合わせて調整してください
 const Editor = dynamic(() => import('./Editor'), {
   ssr: false,
-  loading: () => <p>エディタを読み込み中...</p>,
 });
 
 export default function EditorWrapper() {
