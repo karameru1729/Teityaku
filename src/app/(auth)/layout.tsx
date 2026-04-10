7eed5cca5da5a38cd762aa4b852d5ba51f517d9f
karameru1729 <karameru1729@gmail.com>
2026-04-11 01:54:43 +0900

    add:OAuthの設定
    Googleのアカウント認証画面へのリダイレクトされる機能を作成。細かな認証関係の機能は未実装

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,10 @@
-export default function Authlyout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>){
-  return(
-    <html lang="ja">
-      <body className="bg-white min-h-screen flex flex-col text-gray-900">
-        <header className="py-4 px-6 border-b border-gray-200 flex items-center"> 
-          {children}        
-        </header>
-      </body>
-    </html>
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    // htmlとbodyを取り除き、divなどで囲む
+    <div className="bg-white min-h-screen flex flex-col text-gray-900">
+      <header className="py-4 px-6 border-b border-gray-200 flex items-center"> 
+        {children}        
+      </header>
+    </div>
   );
-} 
+}
