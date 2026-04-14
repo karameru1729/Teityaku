1add905e18e67a6958b83f68c27eecd10f8216c0
karameru1729 <karameru1729@gmail.com>
2026-04-15 01:54:23 +0900

    add:home画面の作成

--- components/RecentAccessButton.tsx
+++ components/RecentAccessButton.tsx
@@ -11,8 +11,8 @@ export default function RecentAccessButton(){
   return(
     <div className="flex flex-col shrink-0 my-2 justify-start bg-zinc-800 border-transparent rounded-xl w-48 h-48 border"> 
       <HomeIcon width={36} height={36} fill="#e2dadad6" className="m-4"/> 
-      <p className="text-xl mx-2">ドキュメント</p>
-      <p className="text-md mt-10 ml-2">1日前</p>
+      <p className="text-xl mx-2 select-none">ドキュメント</p>
+      <p className="text-md mt-10 ml-2 select-none">1日前</p>
     </div>
   );
 }
--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -3,14 +3,20 @@ import SliderBar from "@/../components/SliderBar";
 
 export default function Home() {
   return(
-    <div className="flex flex-col flex-grow justify-start items-center relative overflow-hidden bg-zinc-900">
-      <h1 className="my-16 text-5xl text-center">Teityakuへようこそ</h1>
-
+    <div className="flex flex-col justify-start overflow-auto sideBar-scrollbar items-center sidebar-scrollbar bg-zinc-900">
+      <h1 className="my-16 text-5xl text-center select-none">Teityakuへようこそ</h1>
       <div className="w-1/2 bg-zinc-900">
-        <p className="text-xl py-2">最近のアクセス</p>
+        <p className="text-xl py-2 select-none">最近のアクセス</p>
         <SliderBar/>
       </div>
-
+      <div className="w-1/2 bg-zinc-900">
+        <p className="text-xl py-2 select-none">最近のアクセス</p>
+        <SliderBar/>
+      </div>
+      <div className="w-1/2 bg-zinc-900">
+        <p className="text-xl py-2 select-none">最近のアクセス</p>
+        <SliderBar/>
+      </div>
     </div>
   );
 }
